7251f2e13b0e87989ea7ebcc38a56de497a9a9e1
Mercedes López <glorialopez4578@gmail.com>
2022-10-28 23:34:58 -0600

    :Tarea c

---
+++ Sueldos/Sueldos/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salario
+{
+    class Salario
+    {
+        private int[] sueldos;
+
+        public void Cargar()
+        {
+            sueldos = new int[5];
+            for (int f = 0; f < 5; f++)
+            {
+                Console.Write("Ingrese valor de la componente:");
+                String linea;
+                linea = Console.ReadLine();
+                sueldos[f] = int.Parse(linea);
+            }
+        }
+
+        public void Imprimir()
+        {
+            for (int f = 0; f < 5; f++)
+            {
+                Console.WriteLine(sueldos[f]);
+            }
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            Salario pv = new Salario();
+            pv.Cargar();
+            pv.Imprimir();
+        }
+    }
+}
